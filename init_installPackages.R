1c6418170a206d8a08d313154e62ab1452ed2187
abhayprakash <abhay.prakash1@gmail.com>
2013-11-26 19:02:13 +0530

    local

--- init_installPackages.R
+++ init_installPackages.R
@@ -6,6 +6,9 @@ install.packages("Matrix")
 install.packages("tau")
 install.packages("e1071")
 install.packages("gbm")
-install.packages("randomForest")
+install.packages("randomForest") # neural net
 install.packages("gmodels")
-install.packages("foreach")
+install.packages("foreach") # parallelization in windows
+install.packages("ff")      # for fast on disk object operations
+install.packages("RODBC")   # for big data; data stored in sqlaserve
+install.packages("biglm")   # for logistic regression
