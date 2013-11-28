7846b5651437df8a8a2aebd179ba0dda1dc0e540
Abhay Prakash <abhay.prakash1@gmail.com>
2013-11-28 09:40:23 +0530

    Log Parser to populate DB

---
+++ LogParser/variables.h
@@ -0,0 +1,10 @@
+#ifndef VARIABLES_H_INCLUDED
+#define VARIABLES_H_INCLUDED
+
+const char TRAIN_FILE_PATH[256] = "..\\..\\Data\\train";
+const char TEST_FILE_PATH[256] = "..\\..\\Data\\test";
+
+const char SHORT_TRAIN_FILE_PATH[256] = "..\\..\\Data\\train_short.txt";
+const char SHORT_TEST_FILE_PATH[256] = "..\\..\\Data\\test_short.txt";
+
+#endif // VARIABLES_H_INCLUDED
