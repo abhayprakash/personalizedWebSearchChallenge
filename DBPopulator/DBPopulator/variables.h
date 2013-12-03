6eca2ee2e2e79f7bcb316a2d07f93ce9df92a7a4
Abhay Prakash <abhay.prakash1@gmail.com>
2013-12-03 17:38:07 +0530

    path corrected for lab machine

--- DBPopulator/DBPopulator/variables.h
+++ DBPopulator/DBPopulator/variables.h
@@ -4,7 +4,7 @@
 const char TRAIN_FILE_PATH[256] = "C:\\Users\\Abhay Prakash\\Data\\train";
 const char TEST_FILE_PATH[256] = "C:\\Users\\Abhay Prakash\\Data\\test";
 
-const char SHORT_TRAIN_FILE_PATH[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\train_short.txt";
-const char SHORT_TEST_FILE_PATH[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\test_short.txt";
+const char SHORT_TRAIN_FILE_PATH[256] = "C:\\Users\\Abhay Prakash\\Data\\train_short.txt";
+const char SHORT_TEST_FILE_PATH[256] = "C:\\Users\\Abhay Prakash\\Data\\test_short.txt";
 
 #endif // VARIABLES_H_INCLUDED
