5a538a4ee3ad67a8defd4ee7483598ae486b6fbc
abhayprakash <abhay.prakash1@gmail.com>
2013-12-13 16:14:16 +0530

    started working in VS

--- FeatureExtractor/FeatureExtractor/FeatureExtractor.cpp
+++ FeatureExtractor/FeatureExtractor/FeatureExtractor.cpp
@@ -7,12 +7,26 @@
 
 using namespace std;
 /*
-userid, queryid, urlid, url_position,
+Files required:
+1.
+user_id, query_id, url_id, url_position,
 url_displayed_in_same_session(bool), time_difference_from_most_recent_display, clicked(bool), grade_that_time(0/1/2),
 url_displayed_before_current_session(bool), day_difference_from_most_recent, clicked(bool), grade_that_time,
 similar_query_in_same_session(bool), time_diff_from_most_recent, this_url_shown(bool), grade_that_time,
 similar_query_before_current_session(bool), day_difference_from_most_recent, this_url_shown(bool), grade_that_time,
 grade_this_time
+
+2.
+day, session_id, user_id
+
+3.
+term_id, query_id
+
+4.
+domain_id, url_id
+
+5.
+serp_id, url_id
 */
 struct rowToPrint{
     int user_id;
