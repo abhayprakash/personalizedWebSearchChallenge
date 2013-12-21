49e641ff1534d41a12e2542c9391cf195b8b3291
Abhay Prakash <abhay.prakash1@gmail.com>
2013-12-21 18:21:29 +0530

    code being executed on lab machine

--- FeatureExtractor/FeatureExtractor/variables.cpp
+++ FeatureExtractor/FeatureExtractor/variables.cpp
@@ -2,17 +2,17 @@
 #include "variables.h"
 
 // input files
-char TRAIN_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\verySmallTrain";
-char TEST_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\verySmallTest";
+char TRAIN_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\train";
+char TEST_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\test";
 
 // output files
-char FEATURE_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\feature_train.txt";
-char FEATURE_TEST_PHASE_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\feature_test.txt";
-char FEATURE_VALIDATE_PHASE_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\feature_validate.txt";
+char FEATURE_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\feature_train.txt";
+char FEATURE_TEST_PHASE_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\feature_test.txt";
+char FEATURE_VALIDATE_PHASE_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\feature_validate.txt";
 
-char DOMAIN_URL_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\DU_file.txt";
-char SERP_URL_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\SU_file.txt";
-char QUERY_TERM_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\QT_file.txt";
+char DOMAIN_URL_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\DU_file.txt";
+char SERP_URL_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\SU_file.txt";
+char QUERY_TERM_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\QT_file.txt";
 
-char RESULT_VALIDATE_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\result_validate.txt";
-char RESULT_MAPPER_FILE[256] = "E:\\Projects\\WSDM_PersonalisedWebSearchChallenge\\Data\\result_mapper.txt";
+char RESULT_VALIDATE_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\result_validate.txt";
+char RESULT_MAPPER_FILE[256] = "C:\\Users\\Abhay Prakash\\Data\\Result\\result_mapper.txt";
