d340adeed40a5fa071c7e6286c4b4aaff0522199
Abhay Prakash <abhay.prakash1@gmail.com>
2014-01-05 12:41:33 +0530

    file number to be entered in variables file

--- FeatureExtractor/FeatureExtractor/variables.h
+++ FeatureExtractor/FeatureExtractor/variables.h
@@ -18,6 +18,7 @@ extern char RESULT_VALIDATE_FILE[256];
 extern char RESULT_MAPPER_FILE[256];
 
 #define MB_OR_GB 1024ull
+#define NUM_FILES 10
 
 // memory allocation
 extern unsigned long long BUFF_SIZE_INPUT_READ;
